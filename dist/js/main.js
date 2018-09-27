98332c9e50a1e3d00c28c50625eb35ca129bb97f
Robin Wilkie <robinwilkieglasgow@hotmail.com>
2018-09-27 12:34:10 +0100

    link styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -4,13 +4,13 @@ const menuBtn = document.querySelector(".menu-btn");
 const menu = document.querySelector(".menu");
 const menuNav = document.querySelector(".menu-nav");
 const menuBranding = document.querySelector(".menu-branding");
-const navItems = document.querySelectorAll(".nav-item");
+const navItems = document.querySelectorAll(".nav-item"); //using querySelectorAll to grab all the nav items
 
-// set initial state of menu - open or closed
+// set initial state of menu - open or closed, true or false
 
-let showMenu = false;
+let showMenu = false; // using let rather than const as it needs to change to true
 
-menuBtn.addEventListener("click", toggleMenu);
+menuBtn.addEventListener("click", toggleMenu); // listen for clicking on the burger then fire toggleMenu function
 
 function toggleMenu() {
   if (!showMenu) {
@@ -18,7 +18,7 @@ function toggleMenu() {
     menu.classList.add("show");
     menuNav.classList.add("show");
     menuBranding.classList.add("show");
-    navItems.forEach(item => item.classList.add("show"));
+    navItems.forEach(item => item.classList.add("show")); // loop through each nav link adding show class to each
 
     // set menu state
     showMenu = true;
